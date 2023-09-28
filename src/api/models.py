2a36fdb1623d4044f585d81c85f72ac39abb6fa8
Dmitrii Shmidt <shmicer@gmail.com>
2023-09-28 15:46:06 +0300

    add models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Access(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=255)
+    video_link = models.URLField()
+    products = models.ManyToManyField(Product)
+    duration = models.IntegerField()
+
+class LessonView(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    duration_view = models.IntegerField()
+    is_viewed = models.BooleanField(default=False)
